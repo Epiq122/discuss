470026048e1b38859000da6caca72b3f39e3849f
Robert Gleason <l2ob122@gmail.com>
2023-11-28 21:38:59 -0800

    moved the header auth to its own component and made it a client component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-import { Button } from '@nextui-org/react';
-import * as actions from '@/actions';
-import { auth } from '@/auth';
-import Profile from '@/components/profile';
-import Header from '@/components/header';
-
 export default async function Home() {
-  const session = await auth();
-  return <div></div>;
+  return <div>HomePage</div>;
 }
